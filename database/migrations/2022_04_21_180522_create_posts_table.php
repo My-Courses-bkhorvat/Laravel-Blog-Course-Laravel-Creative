3b9a59c94047a663d8f68e2ca6bededf9a747026
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-23 15:49:00 +0200

    Add categories to posts

--- database/migrations/2022_04_21_180522_create_posts_table.php
+++ database/migrations/2022_04_21_180522_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
+            $table->unsignedSmallInteger('category_id');
             $table->timestamps();
         });
     }
--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -28,48 +28,64 @@
                 <div class="row">
                     <div class="col-12">
                         <form action="{{ route('admin.post.store') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group w-25">
-                            <label for="title">Title</label>
-                            <input type="text" class="form-control" name="title" id="title"
-                                   placeholder="Enter title" value="{{old('title')}}">
-                            @error('title')
-                            <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <textarea id="summernote" name="content">{{old('content')}}</textarea>
-                            @error('content')
-                            <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group w-50">
-                            <label for="exampleInputFile">Add preview</label>
-                            <div class="input-group">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" name="preview_image">
-                                    <label class="custom-file-label">Add image</label>
-                                </div>
-                                <div class="input-group-append">
-                                    <span class="input-group-text">Upload</span>
-                                </div>
+                            @csrf
+                            <div class="form-group w-25">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control" name="title" id="title"
+                                       placeholder="Enter title" value="{{old('title')}}">
+                                @error('title')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
-                        </div>
-                        <div class="form-group w-50">
-                            <label for="exampleInputFile">Add image</label>
-                            <div class="input-group">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" name="main_image">
-                                    <label class="custom-file-label">Add image</label>
-                                </div>
-                                <div class="input-group-append">
-                                    <span class="input-group-text">Upload</span>
-                                </div>
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">{{old('content')}}</textarea>
+                                @error('content')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-primary" value="Create">
-                        </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Add preview</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label">Add image</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Add image</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label">Add image</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Select category</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == old('category_id') ? ' selected' : ''}}
+                                            >{{$category->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Create">
+                            </div>
                         </form>
 
                     </div>
