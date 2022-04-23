59eaa504816175f2bac9ac5cbe6c8cb3d32eed52
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-23 17:54:19 +0200

    Add delete action for posts

--- database/migrations/2022_04_21_180522_create_posts_table.php
+++ database/migrations/2022_04_21_180522_create_posts_table.php
@@ -19,6 +19,7 @@ class CreatePostsTable extends Migration
             $table->text('content');
             $table->unsignedSmallInteger('category_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
