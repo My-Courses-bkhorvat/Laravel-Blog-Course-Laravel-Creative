adc22fbcd5c2227a5abfac6862727a2a41e6d19f
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-23 11:32:58 +0200

    Add crud to tags

--- resources/views/admin/includes/sitebar.blade.php
+++ resources/views/admin/includes/sitebar.blade.php
@@ -2,7 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Sidebar -->
     <div class="sidebar">
-        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
             <li class="nav-item">
@@ -10,6 +11,14 @@
                     <i class="nav-icon fas fa-solid fa-bacon"></i>
                     <p>
                         Category
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('admin.tag.index') }}" class="nav-link">
+                    <i class="nav-icon fas fa-solid fa-tags"></i>
+                    <p>
+                        Tags
                     </p>
                 </a>
             </li>
