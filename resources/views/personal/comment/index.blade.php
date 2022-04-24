4f8ec62e026654beb90d6de70405f30a32c87183
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-24 21:39:08 +0200

    Add bd stuctute for personal comments

--- resources/views/personal/comment/index.blade.php
+++ resources/views/personal/comment/index.blade.php
@@ -25,8 +25,52 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
+                <div class="col-1 mb-3">
+                    <a href="{{ route('admin.post.create') }}" class="btn btn-block btn-primary">Create</a>
+                </div>
+                <div class="col-12">
 
-                <!-- ./col -->
+                </div>
+
+                <div class="col-12">
+                    <div class="card">
+                        <!-- /.card-header -->
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th colspan="2" class="text-center">Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($comments as $comment)
+                                    <tr>
+                                        <td>{{ $comment->id }}</td>
+                                        <td>{{ $comment->message }}</td>
+                                        <td><a href="{{ route('personal.comment.edit', $comment->id) }}"
+                                               class="text-success"><i class="fa-solid fas fa-pen"></i></a></td>
+                                        <td>
+                                            <form action="{{ route('personal.comment.destroy', $comment->id) }}"
+                                                  method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="border-0 bg-transparent">
+                                                    <i class="fa-solid fas fa-trash text-danger" role="buttun"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+
             </div>
             <!-- /.row -->
         </div><!-- /.container-fluid -->
