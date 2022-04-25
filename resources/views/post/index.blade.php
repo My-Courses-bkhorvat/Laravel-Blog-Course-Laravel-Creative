5118f9521c0f674138a30ae74d8f5759488edf1f
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-25 19:31:24 +0200

    Add likes for post to frontend

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -11,7 +11,19 @@
                             <div class="blog-post-thumbnail-wrapper">
                                 <img src="{{ url('storage/' .$post->preview_image) }}" alt="blog post">
                             </div>
-                            <p class="blog-post-category">{{ $post->category->title }}</p>
+                            <div class="d-flex justify-content-between">
+                                <p class="blog-post-category">{{ $post->category->title }}</p>
+                                <form action="{{ route('post.like.store', $post->id) }}" method="post">
+                                    @csrf
+                                    <button type="submit" class="border-0 bg-transparent">
+                                        @if(auth()->user()->likedPosts->contains($post->id))
+                                            <i class="nav-icon fas fa-solid fa-heart"></i>
+                                        @else
+                                            <i class="nav-icon far fa-solid fa-heart"></i>
+                                        @endif
+                                    </button>
+                                </form>
+                            </div>
                             <a href="{{ route('post.show', $post->id) }}" class="blog-post-permalink">
                                 <h6 class="blog-post-title">{{ $post->title }}</h6>
                             </a>
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -18,6 +18,20 @@
             </section>
             <div class="row">
                 <div class="col-lg-9 mx-auto">
+                    <section class="py-3">
+                        <form action="{{ route('post.like.store', $post->id) }}" method="post">
+                            @csrf
+                            <button type="submit" class="border-0 bg-transparent">
+                                @if(auth()->user()->likedPosts->contains($post->id))
+                                    <i class="nav-icon fas fa-solid fa-heart"></i>
+                                @else
+                                    <i class="nav-icon far fa-solid fa-heart"></i>
+                                @endif
+                            </button>
+                        </form>
+                    </section>
+
+                    @if($relatedPosts->count() > 0)
                     <section class="related-posts">
                         <h2 class="section-title mb-4" data-aos="fade-up">Related Posts</h2>
                         <div class="row">
@@ -32,6 +46,7 @@
                             @endforeach
                         </div>
                     </section>
+                    @endif
                         <section class="comment-list mb-5">
                             <h2 class="section-title mb-5" data-aos="fade-up">Comments({{ $post->comments->count() }}
                                 )</h2>
